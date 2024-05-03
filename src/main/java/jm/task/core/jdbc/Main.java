09d763c21db6fcd343f61535c6be1b7fbf5b13c7
madmadmax20k@mail.ru <madmadamax20k@mail.ru>
2024-05-03 17:26:39 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("John", "Doe", (byte) 30);
+        userService.saveUser("Jane", "Doe", (byte) 25);
+        userService.saveUser("Jim", "Beam", (byte) 35);
+        userService.saveUser("Jack", "Daniels", (byte) 40);
+
+        System.out.println("Список всех пользователей:");
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
